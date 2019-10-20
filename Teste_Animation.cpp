482fce213a99098257cb1b714432b9df6c255c8c
KDOXG <kadox0.g@gmail.com>
2019-10-20 12:50:33 -0300

    Update Teste_Animation.cpp

--- Teste_Animation.cpp
+++ Teste_Animation.cpp
@@ -20,82 +20,80 @@ struct control {
 	}
 };
 
-void move(int* action);
+void move(control* action);
 void animation(control* action);
 
 int main(int argc, char* argv[])
 {
 	system("cls");
 	int ch;
-	std::thread anim(move, &ch);
+	control movement = control(0, 0, &ch);
+	animation(&movement);
+	std::thread anim(move, &movement);
 	//pthread_t thread;
 	//pthread_create(&thread, NULL, move, (void*)& ch);
-	while ((ch = getch()) != EOF
-		&& ch != '\n');
+	while ((ch = _getch()) != EOF
+		&& ch != 'e');
 	anim.join();
 	//pthread_join(thread, NULL);
 	return 0;
 }
 
-void move(int* action)
+void move(control* action)
 {
-	int aux;
-	control movement = control(0, 0, action);
+	//int aux;
 	//pthread_t thread;
 	//pthread_create(&thread, NULL, animation, (void*)& movement);
-	std::thread anim(animation, &movement);
+	//std::thread anim(animation, &movement);
 	do
 	{
-		aux = *action;
-			if (aux == '\n')
-				break;
-		switch (aux)
+		system("cls");
+		//aux = *action;
+		if (*action->stop == 'e') break;
+		switch (*action->stop)
 		{
 		case 'w':
-			if (movement.y > 0)
-				movement.y--;
+			if (action->y > 0)
+				action->y--;
 			break;
 		case 'a':
-			if (movement.x > 0)
-				movement.x--;
+			if (action->x > 0)
+				action->x--;
 			break;
 		case 's':
-			if (movement.y < 16)
-				movement.y++;
+			if (action->y < 16)
+				action->y++;
 			break;
 		case 'd':
-			if (movement.x < 16)
-				movement.x++;
+			if (action->x < 16)
+				action->x++;
 			break;
 		default:
 			break;
 		}
+		animation(action);
 		m.lock();
 		//pthread_mutex_lock(&m);
-		*action = 0;
+		*action->stop = 0;
 		m.unlock();
+		std::this_thread::yield();
 		//pthread_mutex_unlock(&m);
-		while (*action == 0) std::this_thread::yield();
+		//while (*action == 0) std::this_thread::yield();
 	} while (1);
-	anim.join();
+	//anim.join();
 	//pthread_join(thread, NULL);
 	return;
 }
 
 void animation(control* action)
 {
-	unsigned long long i;
-	unsigned j, k;
-	for (i = 0; i < ULLONG_MAX && *(action->stop) == 0; i++)
-	{
-		for (k = 0; k < action->y; k++)
-			printf("\n");
-		for (j = 0; j < action->x; j++)
-			printf(" ");
-		printf("O");
-		for (unsigned short n = 0; n < USHRT_MAX; n++);
-		std::this_thread::yield();
-		//sched_yield();
-	}
+	for (unsigned k = 0; k < action->y; k++)
+		printf("\n");
+	for (unsigned j = 0; j < action->x; j++)
+		printf(" ");
+	printf("O");
+	//for (unsigned short n = 0; n < USHRT_MAX; n++);
+	//std::this_thread::yield();
+	//sched_yield();
 	return;
 }
